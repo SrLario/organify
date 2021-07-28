6b6e5c7ff4bd4c1259739da31c82efe3e85747dc
DavidMogo <88087223+DavidMogo@users.noreply.github.com>
2021-07-28 19:15:23 +0200

    Created CalculateGasolineScreen
    - Icons cars
    - Dialog car consumption

--- Organify/app/src/main/java/com/example/organify/CalculateGasolineScreen.java
+++ Organify/app/src/main/java/com/example/organify/CalculateGasolineScreen.java
@@ -2,13 +2,45 @@ package com.example.organify;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
+import android.media.Image;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class CalculateGasolineScreen extends AppCompatActivity {
 
+    //Variables
+    Button carConsumptionButton;
+    ImageView utilitario, compacto, coupe, berlina, familiar, suv, todoterreno, monovolumen, furgoneta;
+
+
+    //Functions
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculate_gasoline_screen);
+        //Car consumption dialog
+        carConsumptionButton = findViewById(R.id.dialog_car_consumption_button);
+        carConsumptionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                carConsumptionDialog();
+            }
+        });
+    }
+
+    //Dialog car consumption
+    public void carConsumptionDialog(){
+        final Dialog createCarConsumptionDialog = new Dialog(CalculateGasolineScreen.this);
+        createCarConsumptionDialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        createCarConsumptionDialog.setContentView(R.layout.pick_consumption_car);
+
+        utilitario = createCarConsumptionDialog.findViewById(R.id.utilitario);
+
+        utilitario.setEnabled(true);
+        createCarConsumptionDialog.show();
     }
 }
