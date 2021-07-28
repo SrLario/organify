21e0f7a2f0b70b047029db61c3cf9db7fca5d310
RogerLario <byroger31@gmail.com>
2021-07-28 17:19:54 +0200

    Calculate gasoline screen

--- Organify/app/src/main/java/com/example/organify/CalculateGasolineScreen.java
+++ Organify/app/src/main/java/com/example/organify/CalculateGasolineScreen.java
@@ -1,4 +1,14 @@
 package com.example.organify;
 
-public class CalculateGasolineScreen {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CalculateGasolineScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculate_gasoline_screen);
+    }
 }
--- Organify/app/src/main/java/com/example/organify/NewTravel.java
+++ Organify/app/src/main/java/com/example/organify/NewTravel.java
@@ -1,14 +1,28 @@
 package com.example.organify;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class NewTravel extends AppCompatActivity {
+
+    ConstraintLayout gasolineButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_travel);
+        gasolineButton = findViewById(R.id.gasoline_button);
+
+        gasolineButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent calculateActivity = new Intent(NewTravel.this, CalculateGasolineScreen.class);
+                startActivity(calculateActivity);
+            }
+        });
     }
 }
