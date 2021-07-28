9c43b104a613faaddb11412ab4b61378c102f199
RogerLario <byroger31@gmail.com>
2021-07-28 16:55:05 +0200

    Created new travel activity
    - Created new travel activity
    - Created all icons
    - Create all buttons
    - Make the design of the activity

--- Organify/app/src/main/java/com/example/organify/HomeScreen.java
+++ Organify/app/src/main/java/com/example/organify/HomeScreen.java
@@ -3,6 +3,8 @@ package com.example.organify;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HomeScreen extends AppCompatActivity {
 
