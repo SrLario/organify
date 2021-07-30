7fdb7fa616de186572a85384f2a4410c5dbe3424
RogerLario <byroger31@gmail.com>
2021-07-30 17:14:44 +0200

    New theme new travel

--- Organify/app/src/main/java/com/example/organify/NewTravel.java
+++ Organify/app/src/main/java/com/example/organify/NewTravel.java
@@ -10,13 +10,18 @@ import android.widget.ImageView;
 
 public class NewTravel extends AppCompatActivity {
 
-    ImageView gasolineButton;
+    ImageView gasolineButton, foodButton, stayButton, peopleButton, activitiesButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_travel);
+
         gasolineButton = findViewById(R.id.gasoline_button);
+        foodButton = findViewById(R.id.food_button);
+        stayButton = findViewById(R.id.stay_button);
+        peopleButton = findViewById(R.id.people_button);
+        activitiesButton = findViewById(R.id.activities_button);
 
         gasolineButton.setOnClickListener(new View.OnClickListener() {
             @Override
